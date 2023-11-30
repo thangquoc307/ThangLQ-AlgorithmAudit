65feb6c463b52a93a7e908145896b52a5fa0dc12
Thắng Lê <lequocthang307@gmail.com>
2023-11-30 10:02:56 +0700

    sgb

--- algorithm/src/Algorithm_5.java
+++ algorithm/src/Algorithm_5.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Algorithm_5 {
     public static void main(String[] args) {
@@ -8,16 +6,15 @@ public class Algorithm_5 {
         List<Integer> list2 = new ArrayList<>(Arrays.asList(new Integer[]{88,99,7,5,2,22,65,89,52,52,32,66}));
         System.out.println(getNumberExists(list1, list2));
     }
-    public static List<Integer> getNumberExists(List<Integer> list1, List<Integer> list2){
-        List<Integer> result = new ArrayList<>();
+    public static Map<Integer, Integer> getNumberExists(List<Integer> list1, List<Integer> list2){
+        Map<Integer, Integer> result = new HashMap<>();
         for (Integer number : list1){
-
+            if (result.containsKey(number)){
+                result.put(number, result.get(number) + 1);
+            } else {
+                result.put(number, 1);
+            }
         }
-
-
-
-
-
         return result;
     }
 }
