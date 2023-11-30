86903b93aa10aa7daaedd900011d7bbff42614b2
Thắng Lê <lequocthang307@gmail.com>
2023-11-30 10:19:19 +0700

    sgb

--- algorithm/src/Algorithm_14.java
+++ algorithm/src/Algorithm_14.java
@@ -1,2 +1,17 @@
+import java.time.LocalTime;
+
 public class Algorithm_14 {
+    public static void main(String[] args) {
+        String a1 = "11:20:30";
+        String a2 = "07:35:45";
+        System.out.println(sumTime(a1, a2));
+
+    }
+    public static String sumTime(String a1, String a2){
+        String[] arr1 = a1.split(":");
+        String[] arr2 = a2.split(":");
+        LocalTime time1 = LocalTime.of(Integer.parseInt(arr1[0]), Integer.parseInt(arr1[1]), Integer.parseInt(arr1[2]));
+        LocalTime result = time1.plusHours(Long.parseLong(arr2[0])).plusMinutes(Long.parseLong(arr2[1])).plusSeconds(Long.parseLong(arr2[2]));
+        return result + "";
+    }
 }
