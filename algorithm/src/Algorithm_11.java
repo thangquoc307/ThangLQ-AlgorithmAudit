65f55dc91d6b0b882d67ac2da8cbd64f833b569b
Thắng Lê <lequocthang307@gmail.com>
2023-12-01 08:22:58 +0700

    sgb

--- algorithm/src/Algorithm_11.java
+++ algorithm/src/Algorithm_11.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Algorithm_11 {
+    public static void main(String[] args) {
+        List<Integer> fibonacci = new ArrayList<>();
+        fibonacci.add(0);
+        fibonacci.add(1);
+        while (true){
+            int i = fibonacci.get(fibonacci.size() - 1) + fibonacci.get(fibonacci.size() - 2);
+            if (i < 100) {
+                fibonacci.add(i);
+            } else {
+                break;
+            }
+        }
+        System.out.println(fibonacci);
+    }
 }
