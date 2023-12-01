b31929c576501dc0a81797e26a81560b7544acc1
Thắng Lê <lequocthang307@gmail.com>
2023-12-01 08:29:35 +0700

    sgb

--- algorithm/src/Algorithm_19.java
+++ algorithm/src/Algorithm_19.java
@@ -1,2 +1,13 @@
 public class Algorithm_19 {
+    public static void main(String[] args) {
+        String str = "gfdsbnyasghbs";
+        System.out.println(reverseString(str));
+    }
+    private static String reverseString (String str){
+        String result = "";
+        for (int i = str.length() - 1; i >= 0; i--){
+            result += ("" + str.charAt(i));
+        }
+        return result;
+    }
 }
