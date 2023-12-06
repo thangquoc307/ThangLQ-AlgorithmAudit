9b5f80644cc07b5ba7841fd8ad50c010244a2735
Thắng Lê <lequocthang307@gmail.com>
2023-12-06 08:52:27 +0700

    ágn

--- algorithm/src/Algorithm_13.java
+++ algorithm/src/Algorithm_13.java
@@ -1,2 +1,29 @@
 public class Algorithm_13 {
+    public static void main(String[] args) {
+        System.out.println(convertNumber(2));
+        System.out.println(convertNumber(3));
+        System.out.println(convertNumber(8));
+        System.out.println(convertNumber(16));
+        System.out.println(convertNumber(17));
+        System.out.println(convertNumber(18));
+        System.out.println(convertNumber(800));
+        System.out.println(convertNumber(0));
+
+    }
+    public static Integer convertNumber (Integer num) {
+        if (num == 0) {
+            return 0;
+        }
+
+        int result = 0;
+        int count = 0;
+
+        while (num > 0) {
+            result = result + (num % 8) * (int) Math.pow(10, count);
+            num /= 8;
+            count++;
+        }
+
+        return result;
+    }
 }
